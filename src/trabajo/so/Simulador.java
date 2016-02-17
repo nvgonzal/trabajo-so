caa0adfd6c2805774c3208831ac0a0aee700e200
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-02-17 16:55:44 -0300

    Crea clase de simulador de proceso

---
+++ src/trabajo/so/Simulador.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajo.so;
+
+import java.util.ArrayList;
+import javax.swing.JTabbedPane;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Simulador {
+    
+    public JTabbedPane panelPestañas;
+    public ArrayList<Proceso> listaProcesos;
+    
+    
+    public Simulador(JTabbedPane panelPestañas){
+        this.panelPestañas=panelPestañas;
+    }
+    
+    public void simularProceso(){
+        
+    }
+    
+    
+    
+    
+}
