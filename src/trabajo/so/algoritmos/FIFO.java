91b912ad23ee1fcb34c7976696e7a2c5d47c3af7
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-02-17 17:04:15 -0300

    Crea clase para algoritmo de administracion de procesos FIFO

---
+++ src/trabajo/so/algoritmos/FIFO.java
@@ -0,0 +1,21 @@
+package trabajo.so.algoritmos;
+
+import java.util.ArrayList;
+import trabajo.so.Proceso;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class FIFO {
+    
+    public ArrayList<Proceso> listaProcesos;
+    public String cartaGant;
+    
+    
+    public FIFO(ArrayList<Proceso> listaProcesos){
+        this.listaProcesos = listaProcesos;
+    }
+    
+    
+}
